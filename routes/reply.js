685196fee5d45473cbabe58eabfd8d6c2f4f1151
Kerem ERSOY <keremersoy2001@gmail.com>
2023-01-29 23:19:12 +0300

    reply rating processes

--- routes/reply.js
+++ routes/reply.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Reply = require("../models/Reply");
+const Vote = require("../models/Vote");
 const Joi = require("joi");
 
 const replySchema = Joi.object({
@@ -55,21 +56,87 @@ router.get("/get/:parent_id", (req, res) => {
 });
 
 router.delete("/delete/:id", (req, res) => {
-    const { id } = req.params;
-  
-    Reply.findByIdAndDelete(id)
-      .then((reply) => {
-        res.status(200).json({
-          success: true,
-          data: reply,
-        });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          success: false,
-          message: error,
-        });
+  const { id } = req.params;
+
+  Reply.findByIdAndDelete(id)
+    .then((reply) => {
+      res.status(200).json({
+        success: true,
+        data: reply,
       });
-  });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: error,
+      });
+    });
+});
+
+router.post("/add/user/vote/:replyId", (req, res) => {
+  const replyId = req.params.replyId;
+  const vote = {
+    replyId: replyId,
+    userId: req.userId,
+    vote: req.body.vote,
+  };
+  const result = Vote.findOneAndUpdate(
+    { replyId: replyId, userId: req.userId },
+    vote,
+    { upsert: true, new: true }
+  )
+    .then((v) => {
+      res.status(200).json({
+        success: true,
+        data: v,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: error,
+      });
+    });
+});
+
+router.get("/get/user/vote/:replyId", (req, res) => {
+  const replyId = req.params.replyId;
+
+  Vote.find({ replyId: replyId, userId: req.userId })
+    .then((vote) => {
+      res.status(200).json({
+        success: true,
+        data: vote[0].vote,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: 0,
+      });
+    });
+});
+
+router.get("/get/rating/:replyId", (req, res) => {
+  const replyId = req.params.replyId;
+
+  Vote.find({ replyId: replyId })
+    .then((votes) => {
+      let total = 0;
+      votes.forEach((item) => {
+        total += item.vote;
+      });
+      res.status(200).json({
+        success: true,
+        data: total,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: total,
+      });
+    });
+});
 
 module.exports = router;
